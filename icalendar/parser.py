b20f3f840df305d503a66de561bf855c597907c6
Eryn Wells <eryn@erynwells.me>
2012-05-03 12:27:51 -0700

    Lex an iCalendar object with the power of regexes
    Define all the tokens of the language in langspec.py. The lexing is done in
    parser.py.

--- icalendar/parser.py
+++ icalendar/parser.py
@@ -1,9 +1,4 @@
-#
-#
-#
-
-'''
-An iCalendar object parser.
+'''An iCalendar object parser. {{{
 
 From RFC 2445:
 
@@ -50,17 +45,15 @@ Parsing proceeds in the following manner:
     1. Read a line (until a CRLF *not* followed by a WSP character)
     2. Unfold if necessary (remove CRLF WSP sequences)
     3. Parse content line
-'''
+}}}'''
 
 
-from StringIO import StringIO
+from __future__ import print_function
+import langspec
 
 
-class ICalendarObject:
+class ICalendarObject(list):
     'An iCalendar object.'
-
-    def __init__(self):
-        self.content_lines = []
 
 
 class ICalendarContentLine:
@@ -71,18 +64,57 @@ class ICalendarContentLine:
         self.value = value
         self.params = params if params is not None else []
 
+    def __str__(self):
+        param_string = ';'.join(self.params)
+        return '{0.name}{1}{2}:{0.value}{3}'.format(self,
+                ';' if param_string != '' else '', param_string,
+                langspec.CRLF.pattern)
+
 
 def parse(data):
-    'Parse raw_data into an ICalendarObject and return it.'
-    if hasattr(data, 'read'):
-        # Stream
-        pass
-    elif issubclass(data, basestring):
-        data = StringIO(data)
-    else:
+    '''
+    Parse raw_data into an ICalendarObject and return it.
+
+    @param data: The data to parse (subclass of basestring)
+    @returns: The parsed data (object)
+    @raises: TypeError on invalid input
+    '''
+    if not issubclass(data, basestring):
         raise TypeError('Invalid type {} for incoming data'.format(
                         type(data).__name__))
-    return _parse_stream(data)
+    return _lex_ical_object(data)
 
-def _parse_stream(stream):
-    pass
+
+def _lex_ical_object(data):
+    '''
+    Lex the iCalendar object into a top-level ICalendarObject and a list of
+    ICalendarContentLine objects. The result has no semantic meaning, it's just
+    tokenized.
+
+    @param data: The data to lex (str)
+    @returns: The lexed data (ICalendarObject)
+    '''
+    obj = ICalendarObject()
+    lines = langspec.CRLF.split(_unfold(data))
+    for line in lines:
+        print(line)
+        m = langspec.CONTENT_LINE.search(line)
+        if m is None:
+            raise RuntimeError('Invalid iCalendar line: {}'.format(line))
+        line_components = dict(m.groupdict())
+        params = line_components['params']
+        if params == '':
+            line_components['params'] = []
+        else:
+            # First element of the list will always be '' because of how the
+            # regex extracts the param list
+            params = params.split(';')[1:]
+            line_components['params'] = params
+        obj.append(ICalendarContentLine(**line_components))
+    return obj
+
+
+def _unfold(data):
+    '''Perform any necessary unfolding on the incoming data. Return the unfolded
+    data.'''
+    return langspec.FOLD_MARKER.sub('', data)
