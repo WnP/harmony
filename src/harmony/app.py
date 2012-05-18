c0c4705e74c9363f60530baa8ccb1ca3297233dd
Eryn Wells <eryn@erynwells.me>
2012-05-18 15:05:27 +0100

    Bit of reorg of the app parameters
    Add some comments
    Rename some of the settings

--- src/harmony/app.py
+++ src/harmony/app.py
@@ -3,9 +3,14 @@ Basic app data.
 '''
 
 
-AUTHOR = 'Eryn Wells'
-NAME = 'Harmony'
-VERSION = (0, 1)
-VERSION_STRING = '.'.join([str(v) for v in VERSION])
+# Harmony parameters
+AUTHOR = u'Eryn Wells'
+NAME = u'Harmony'
+VERSION = (0, 0, 1)
+VERSION_STRING = u'{}.{}{}'.format(VERSION)
 
-ICALENDAR_VERSION = '2.0'
+# iCalendar parameters
+ICAL_PRODID = u'-//{author}//{name} {version}//EN'.format(
+        author=AUTHOR, name=NAME, version=VERSION_STRING)
+ICAL_VERSION = (2, 0)
+ICAL_VERSION_STRING = u'.'.join(ICAL_VERSION)
