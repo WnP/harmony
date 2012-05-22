9ec26534478a42522243dc0ab548dc24d9e249dc
Eryn Wells <eryn@erynwells.me>
2012-05-22 12:52:01 +0100

    Prefix app-wide variables with descriptive prefixes

--- src/harmony/app.py
+++ src/harmony/app.py
@@ -2,15 +2,22 @@
 Basic app data.
 '''
 
+from os.path import expanduser as path_expanduser, join as path_join
+
 
 # Harmony parameters
-AUTHOR = u'Eryn Wells'
-NAME = u'Harmony'
-VERSION = (0, 0, 1)
-VERSION_STRING = u'{}.{}{}'.format(VERSION)
+APP_AUTHOR = u'Eryn Wells'
+APP_NAME = u'Harmony'
+APP_VERSION = (0, 0, 1)
+APP_VERSION_STRING = u'{}.{}{}'.format(VERSION)
 
 # iCalendar parameters
 ICAL_PRODID = u'-//{author}//{name} {version}//EN'.format(
         author=AUTHOR, name=NAME, version=VERSION_STRING)
 ICAL_VERSION = (2, 0)
 ICAL_VERSION_STRING = u'.'.join(ICAL_VERSION)
+
+# Configuration parameters
+CONFIG_DIRECTORY = path_join(path_expanduser('~'), '.harmony')
+CONFIG_HARMONY = path_join(CONFIG_DIRECTORY, 'harmony.conf')
+CONFIG_CALENDARS_DB = path_join(CONFIG_DIRECTORY, 'calendars.db')
