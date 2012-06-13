1ae152051ec7b1857d58e9a1cd3e06bd2eb460e9
Eryn Wells <eryn@erynwells.me>
2012-06-12 20:33:54 -0700

    Models like Django

--- src/harmony/calendar.py
+++ src/harmony/calendar.py
@@ -6,76 +6,28 @@ Functions for processing calendars.
 from pytz import timezone as pytz_timezone
 from random import randint
 
-
-class PersistableObject(object):
-    def __init__(self, pk=None):
-        self.pk = pk
-
-    @classmethod
-    def create(cls, **kwargs):
-        '''
-        Create a new instance of cls.
-
-        @param kwargs: Keyword arguments to pass to the model (dict)
-        @returns: The new object (cls)
-        '''
-        newobj = cls(**kwargs)
-        newobj.save()
-        return newobj
-
-    def save(self):
-        self.pk = randint(0, 65536)
-
-    def __repr__(self):
-        return "<{0.__class__.__name__} '{0}'>".format(self)
-
-    def __str__(self):
-        return str(unicode(self))
+from .persistence import model
 
 
-class Calendar(PersistableObject):
+class Calendar(model.Model):
     '''A calendar.'''
 
-    DEFAULT_NAME = u'Untitled Calendar'
-
-    def __init__(self, name=DEFAULT_NAME, timezone=None, default=False,
-                 **kwargs):
-        '''
-        @param name: Name of this calendar (str)
-        @param timezone: Timezone of this calendar (str)
-        '''
-        super(Calendar, self).__init__(**kwargs)
-        self.name = name
-        self.timezone = timezone
-        self.default = default
-        self._events = []
+    name = model.TextField(unique=True, default='Untitled Calendar')
+    timezone = model.TimezoneField()
+    is_default = model.BooleanField(default=False)
 
     def __unicode__(self):
         return unicode(self.name)
 
-    def add_event(self, event):
-        self._events.append(event)
 
+class Event(model.Model):
+    '''An event.'''
 
-class Event(PersistableObject):
-    '''An event. Events are individual entries in a calendar. An event can only
-    exist as part of, or paired with, a Calendar object.'''
-
-    DEFAULT_SUMMARY = u'Untitled event'
-
-    def __init__(self, summary=DEFAULT_SUMMARY, start=None, end=None,
-                 calendar=None, **kwargs):
-        '''
-        @param summary: The event's description, title, summary, whatever. (str)
-        @param start: The start time of the event. (datetime)
-        @param end: The end time of the event. (datetime)
-        @param calendar: The calendar this event is associated with (Calendar)
-        '''
-        super(Event, self).__init__(**kwargs)
-        self.summary = summary
-        self.start = start
-        self.end = end
-        self.calendar = calendar
+    summary = model.TextField(default='Untitled event')
+    all_day = model.BooleanField(default=False)
+    start = model.DateTimeField()
+    end = model.DateTimeField()
+    calendar = model.ForeignKeyField(Calendar)
 
     def __unicode__(self):
         return unicode(self.summary)
