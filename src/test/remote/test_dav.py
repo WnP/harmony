1ca1cc36cd6752e2ed8d5448c9d3ffd6de0765a4
Eryn Wells <eryn@erynwells.me>
2013-03-04 20:46:02 -0800

    Add test for _split_namespace method on dav.RootParser

--- src/test/remote/test_dav.py
+++ src/test/remote/test_dav.py
@@ -1,4 +1,24 @@
+'''
+Test cases for harmony.remote.dav.
+'''
+
+import unittest
+import harmony.remote.dav as dav
 
 
-def test():
-    assert False
+class TestCalDAVClient(unittest.TestCase):
+    pass
+
+
+class TestRootParser(unittest.TestCase):
+    def setUp(self):
+        self.parser = dav.RootParser()
+
+    def test_positive_namespace_split(self):
+        ns, tag = self.parser._split_namespace('caldav:calendar')
+        self.assertTrue(ns == 'caldav')
+        self.assertTrue(tag == 'calendar')
+
+        ns, tag = self.parser._split_namespace('foo')
+        self.assertTrue(ns == '')
+        self.assertTrue(tag == 'foo')
